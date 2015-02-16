77609d853a521f053d01c7377b6b719f169ce41f
Justin <justinwang764@gmail.com>
2015-02-16 15:13:32 -0600

    Ball Project
    Basic ball game

---
+++ Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	private int count;
+	public GUIText countText;
+	public GUIText winText;
+
+	void Start ()
+	{
+		count = 0;
+		SetCount ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed *Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCount();
+		}
+	}
+	void SetCount()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 15)
+			winText.text = "YOU WIN!";
+	}
+}
